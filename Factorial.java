c29bfb11698b5a8f87aa37c356a6f69434437ed1
Jhelsing1 <43977262+Jhelsing1@users.noreply.github.com>
2018-10-30 12:25:03 -0700

    kjawda

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+
+public class Factorial
+{
+    public static void main(String[]args)
+    {
+        int ans=factorial(5);
+        System.out.println(ans);
+    }
+    public static int factorial(int n)
+    {
+        System.out.println("DEBUG" +n);
+        if(n<=1)
+        return 1;
+        return factorial(n-1);
+    }
+}
